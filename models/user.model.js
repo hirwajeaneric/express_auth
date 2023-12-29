93df6f3b2c6bf05b172edc3ab129b0c97f46102a
hirwajeaneric <hirwajeaneric25@gmail.com>
2023-12-29 11:23:07 +0200

    User model

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    fullName: {
+        type: String, 
+        required: true,
+    },
+    email: {
+        type: String, 
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String, 
+        required: true,
+    },
+    profilePicture: {
+        type: String, 
+        required: true,
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = model('User', UserSchema);
